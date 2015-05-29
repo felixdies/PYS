94c05425c32b64cc40e9e972768d82f5cee90f34
A <A>
2015-05-30 00:36:28 +0900

    first commit - create fio

---
+++ fio.py
@@ -0,0 +1,22 @@
+"""File IO Handler
+read text from fio_in.txt
+write result to fio_out.txt
+"""
+
+__author__ = 'felixdies'
+
+
+def readAllText():
+    pass
+
+
+def readAllLines():
+    pass
+
+
+def readAllSplitText():
+    pass
+
+
+def readAllSplitLines():
+    pass
